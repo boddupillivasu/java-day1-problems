9b43a7a86c54a7982145bbabb7696aa9110cf599
boddupillivasu <boddupillibujji123@gmail.com>
2025-03-19 18:50:16 +0530

    chore: print product of float value

---
+++ src/com/week1/codes/ProductOfTwoFlotNum.java
@@ -0,0 +1,16 @@
+package com.week1.codes;
+
+import java.util.Scanner;
+
+public class ProductOfTwoFlotNum {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter first value:");
+        float val = scanner.nextFloat();
+        System.out.println("enter second value:");
+        float value = scanner.nextFloat();
+        System.out.println("the product of float value:"+(value*value));
+    }
+}
