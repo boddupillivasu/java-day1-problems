23e49b1134242ee893647fb9fbad46c478aed084
boddupillivasu <boddupillibujji123@gmail.com>
2025-03-19 18:49:03 +0530

    chore: print char

---
+++ src/com/week1/codes/PrintChar.java
@@ -0,0 +1,21 @@
+package com.week1.codes;
+
+import java.util.Scanner;
+
+public class PrintChar {
+
+    public static void printChar(){
+
+        Scanner scanner = new Scanner(System.in);
+        char value = scanner.next().charAt(0);//index position
+        System.out.println("the char value:"+value);
+    }
+
+    public static void main(String[] args) {
+//        printChar();
+        Scanner scanner = new Scanner(System.in);
+        char value = scanner.next().charAt(8                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      );//index position
+        System.out.println("the char value:"+value);
+
+    }
+}
