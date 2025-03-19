8584e7e61cee14f559708cc4886ac26dbf683f2b
boddupillivasu <boddupillibujji123@gmail.com>
2025-03-19 18:49:16 +0530

    chore: print float

---
+++ src/com/week1/codes/PrintFloatingPointNumber.java
@@ -0,0 +1,24 @@
+package com.week1.codes;
+
+import java.util.Scanner;
+
+public class PrintFloatingPointNumber {
+
+    public static void printFlot() {
+        Scanner scanner = new Scanner(System.in);
+        float number = scanner.nextFloat();
+        System.out.println("the Flot value"+number);
+
+    }
+
+    public static void main(String[] args) {
+//        printFlot();
+        Scanner scanner = new Scanner(System.in);
+        float number = scanner.nextFloat();
+        System.out.println("the Flot value"+number);
+
+
+
+
+    }
+}
