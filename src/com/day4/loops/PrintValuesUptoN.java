8a184cca8ab9d53638eacdbf1cd335b197ec9c90
boddupillivasu <boddupillibujji123@gmail.com>
2025-03-27 19:18:11 +0530

    chore:PrintValuesUptoN

---
+++ src/com/day4/loops/PrintValuesUptoN.java
@@ -0,0 +1,14 @@
+package com.day4.loops;
+
+public class PrintValuesUptoN {
+
+    public static void main(String[] args) {
+        int n =100 ;
+
+        for (int i = 1;i<=n; i++){
+
+            System.out.println(i+"");
+        }
+        System.out.println();
+    }
+}
